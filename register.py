e2e7a518848d097e18f2678cd370610c62e6bc2e
Mac0r0ni_And_Weed <61136278+Mac0r0ni@users.noreply.github.com>
2022-03-09 13:36:27 -0500

    Update register.py

--- register.py
+++ register.py
@@ -1,6 +1,6 @@
 import logging
 import time, sys, json
-from kik_unofficial.client import KikClient
+from kik_unofficial.client impoort KikClient
 from kik_unofficial.callbacks import KikClientCallback
 from kik_unofficial.datatypes.xmpp.errors import SignUpError
 from kik_unofficial.datatypes.xmpp.login import LoginResponse
@@ -15,7 +15,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 
-HOST, PORT = "talk1110an.kik.com", 5223
+HOST, PORT = "talk11110an.kik.com", 5223
 log = logging.getLogger('kik_unofficial')
 
 captchas = []
@@ -51,75 +51,7 @@ class FetchRosterResponse(XMPPResponse):
             raise KikParsingException("Unsupported peer element tag: {}".format(element.name))
 
 
-def register(email, username, password, first_name, last_name, birthday="1974-11-20", captcha_result=None):
-    """
-    Sends a register request to sign up a new user to kik with the given details.
-    """
-
-    def main():  # This function is used to execute the login process only if the file was run directly, and not imported.
-        RegisterClient()
-
-   
-        def on_sign_up_ended(self, response: RegisterResponse):
-            global waiting_for_captcha
-            waiting_for_captcha = False
-            print("\nAccount already exists!!!")
-            print("Registered on node {}.".format(response.kik_node))
-            print("\n")
-
-        def on_authenticated(self):
-            print("Authorized connection initiated.")
-            self.client.request_roster()
-
-        def on_login_ended(self, response: LoginResponse):
-            print("Logged in as {}.".format(response.username))
-
-        def on_register_error(self, response: SignUpError):
-            if "captcha_url" in dir(response):
-                print("\nCaptcha URL:", response.captcha_url)
-
-                opt = webdriver.ChromeOptions()
-                opt.add_argument("start-maximized")
-                #opt.add_argument("--auto-open-devtools-for-tabs")
-                mobile_emulation = {"deviceName": "iPhone X"}
-                # opt.headless = True
-                # opt.add_experimental_option("excludeSwitches", ["enable-logging"])
-                opt.add_argument('log-level=3')
-                opt.add_experimental_option("mobileEmulation", mobile_emulation)
-
-                # make chrome log requests
-                capabilities = DesiredCapabilities.CHROME
-                capabilities["goog:loggingPrefs"] = {"performance": "ALL"}  # newer: goog:loggingPrefs
-
-                driver = webdriver.Chrome('C:\PATH_TO_CHROMEDRIVER.EXE_HERE', desired_capabilities=capabilities, chrome_options=opt)
-                driver.get(response.captcha_url)
-
-                print("\n\n----------------------------------------------------------------------------")
-                inp = input("Are you done? type anything and press enter once you are done.\n")
-                while not inp:
-                    time.sleep(2)
-                # extract requests from logs
-                logs_raw = driver.get_log("performance")
-                logs = [json.loads(lr["message"])["message"] for lr in logs_raw]
-                driver.close()
-                def log_filter(log_):
-                    return (
-                        # is an actual response
-                            log_["method"] == "Network.requestWillBeSent"
-                            # and json
-                            and "captcha-url?response=" in log_["params"]["documentURL"]
-                    )
-
-                reqlist = filter(log_filter, logs)
-                result = list(reqlist)[0]["params"]["request"]["url"].split("response=")[1]
-
-                print("Captcha Respnse Fetced...")
-
-
-                global captchas
-                captchas.append([email, username, password, first_name, last_name, birthday, response.captcha_url])
-
-                # result = input("Captcha result:")
+def
                 global waiting_for_captcha
                 waiting_for_captcha = False
 
@@ -155,17 +87,7 @@ with open('register.csv', 'r') as read_obj:
             if not row[j].strip():
                 print(header[j], " does not have a valid value on line", i, "in register.csv")
 
-        # row variable is a list that represents a row in csv
-        in_email = row[0].strip()
-        in_user = row[1].strip()
-        in_pass = row[2].strip()
-        in_first_name = row[3].strip()
-        in_last_name = row[4].strip()
-        in_birthday = row[5].strip()
-
-        register(in_email, in_user, in_pass, in_first_name, in_last_name, in_birthday)
-        waiting_for_captcha = True
-
+    
         while waiting_for_captcha:
             time.sleep(5)
 
